30a0794cc6ef2be74bf63cc1eb29e952aa3c7496
Chetanya2001 <mathurchetanya1@gmail.com>
2025-09-21 22:17:59 +0530

    route updated

--- src/routes/App.routes.tsx
+++ src/routes/App.routes.tsx
@@ -5,6 +5,7 @@ import CarWizard from "../components/AddCarWizard/AddCarWizard";
 import SearchedCars from "../pages/searchedCars/searchedCars";
 import MyCars from "../pages/MyCars/MyCars";
 import CarDetails from "../pages/CarDetails/CarDetails";
+import VerifyEmail from "../pages/verify-email/verify-email";
 
 const AppRoutes = () => (
   <BrowserRouter>
@@ -15,6 +16,7 @@ const AppRoutes = () => (
       <Route path="/searched-cars" element={<SearchedCars />} />
       <Route path="/my-cars" element={<MyCars />} />
       <Route path="/car-details/:id" element={<CarDetails />} />
+      <Route path="/verify-email" element={<VerifyEmail />} />
       {/* other routes */}
     </Routes>
   </BrowserRouter>
