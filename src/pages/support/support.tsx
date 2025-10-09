d6f438db070130a64a52cf0e54a33d90a0ba961e
Chetanya2001 <mathurchetanya1@gmail.com>
2025-10-09 16:42:03 +0530

    new support again

--- src/pages/support/support.tsx
+++ src/pages/support/support.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
-import "./Support.css";
+import "./support.css";
 // Add your CSS according to your design system
 
 export default function Support() {
