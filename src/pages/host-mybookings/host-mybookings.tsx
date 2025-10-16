15b18549058b6be782ef1297063b40a705970740
Chetanya2001 <mathurchetanya1@gmail.com>
2025-10-16 15:42:18 +0530

    imprt

--- src/pages/host-mybookings/host-mybookings.tsx
+++ src/pages/host-mybookings/host-mybookings.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import { getHostBookings } from "../../services/booking";
-import "./host-mybooking.css";
+import "./host-mybookings.css";
 
 type HostBooking = {
   id: number;
