bd2d8234b9099b74575cded533f10b876208832b
Chetanya2001 <mathurchetanya1@gmail.com>
2025-12-01 14:58:57 +0530

    added js bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "leaflet/dist/leaflet.css";
 
 import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
